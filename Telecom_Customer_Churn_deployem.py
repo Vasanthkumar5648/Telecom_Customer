e369db9f071cace879fd75fa8f85ff0dbef1ed03
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-03-24 14:47:02 +0530

    Update Telecom_Customer_Churn_deployem.py

--- Telecom_Customer_Churn_deployem.py
+++ Telecom_Customer_Churn_deployem.py
@@ -81,5 +81,5 @@ if prediction[0] == 0:
     st.markdown('<div class="prediction-box" style="color: Blue;">✅ This customer is likely to stay.</div>', unsafe_allow_html=True)
 else:
     st.markdown('<div class="prediction-box" style="color: Red;">⚠️ This customer is likely to churn.</div>', unsafe_allow_html=True)
-except Exception as e:
+except as e:
         st.error(f"Prediction Error: {e}")
